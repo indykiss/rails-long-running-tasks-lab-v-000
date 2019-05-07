f29481a0883cf338fe57675a4bec7ef802c2781f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-07 19:04:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :songs, only: [:index]
-  post 'songs/upload', to: 'songs#upload'
+  resources :songs
+  resources :artists
 
-  resources :artists, only: [:index]
-  post 'artists/upload', to: 'artists#upload'
 
 end
