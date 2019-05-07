503ecdc4ad10095d10c8d8d73a4606e3a9941d0a
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-07 19:01:09 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,7 +6,7 @@ class ArtistsController < ApplicationController
   end
 
   def upload 
-    CSV.foreach(params[:files].path, headers: true) do |lead|
+    CSV.foreach(params[:files].path, headers: true) do |song|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
     redirect_to customers_path
